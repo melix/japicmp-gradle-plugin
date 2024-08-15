ce9d6a631606a3107161630015f75e4ffcd0cc69
Goooler <wangzongler@gmail.com>
2024-08-15 10:59:48 +0800

    Polish test matrix and disable older JDKs for Gradle 8.10

--- build-logic/src/main/kotlin/me.champeau.convention-testing.gradle.kts
+++ build-logic/src/main/kotlin/me.champeau.convention-testing.gradle.kts
@@ -20,15 +20,30 @@ tasks.test {
     onlyIf { false }
 }
 
-val allGradle = listOf("6.6", "7.3", "7.6", GradleVersion.current().version)
+// Test on older Gradle and the latest one.
+val allGradle = listOf(
+    GradleVersion.version("6.6"),
+    GradleVersion.version("7.3"),
+    GradleVersion.version("7.6"),
+    GradleVersion.current(),
+)
+// Configure various JDKs on CI.
 val testJdk = providers.gradleProperty("me.champeau.japicmp.javaToolchain.test")
     .getOrElse("8").toInt()
 
 // https://docs.gradle.org/current/userguide/compatibility.html
-allGradle.forEach { gradleVersion ->
-    if (gradleVersion < "7.3" && testJdk >= 17) return@forEach
-    if (gradleVersion < "8.5" && testJdk >= 21) return@forEach
-    if (gradleVersion < "8.8" && testJdk >= 22) return@forEach
+allGradle.forEach { gv ->
+    // Gradle 7.3+ supports Java 17.
+    if (gv < GradleVersion.version("7.3") && testJdk >= 17) return@forEach
+    // Gradle 8.5+ supports Java 21.
+    if (gv < GradleVersion.version("8.5") && testJdk >= 21) return@forEach
+    // Gradle 8.8+ supports Java 22.
+    if (gv < GradleVersion.version("8.8") && testJdk >= 22) return@forEach
+    // Executing Gradle on JVM versions 16 and lower has been deprecated. Use JVM 17 or greater to execute Gradle.
+    // See https://docs.gradle.org/8.10/userguide/upgrading_version_8.html#minimum_daemon_jvm_version.
+    if (gv >= GradleVersion.version("8.10") && testJdk < 17) return@forEach
+
+    val gradleVersion = gv.version
 
     val task = tasks.register<Test>("testJdk${testJdk}onGradle${gradleVersion}") {
         group = LifecycleBasePlugin.VERIFICATION_GROUP
